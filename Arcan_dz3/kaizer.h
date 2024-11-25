0ac264bd946a8bad92adb7a0b6f5ebd8c3f8696c
Ka1zerr <guzoban@gmail.com>
2024-11-25 22:20:12 +0300

    Delete Arcan_dz3 directory

--- Arcan_dz3/kaizer.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <cstdio>
-int sumOfNumerals(int number); 
-const char* getArcana(int arc);
-void displayLog(FILE* log);
-void vivodArcana(FILE* log, FILE* info);
-void menu(FILE* log, FILE* info);
