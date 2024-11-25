dba7f5b9bdb074e1d86051c57f2641d4b4a6836e
Ka1zerr <guzoban@gmail.com>
2024-11-25 22:21:20 +0300

    Дз 4

---
+++ Arcan_dz3/kaizer.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <cstdio>
+int sumOfNumerals(int number); 
+const char* getArcana(int arc);
+void displayLog(FILE* log);
+void vivodArcana(FILE* log, FILE* info);
+void menu(FILE* log, FILE* info);
